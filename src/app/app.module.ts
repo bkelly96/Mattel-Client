d0e08c6411251c4771d1c4e8bbf2aab3ba78681b
Beekenstein <bkelly0905@gmail.com>
2021-09-06 08:33:45 -0400

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ReviewRequestComponent } from './review-request/review-request.component';
+import { SubmitComponent } from './submit/submit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ReviewRequestComponent,
+    SubmitComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/review-request/review-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-review-request',
+  templateUrl: './review-request.component.html',
+  styleUrls: ['./review-request.component.css']
+})
+export class ReviewRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
